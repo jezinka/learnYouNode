71dbb00d8626f39880bfa1b1865e33ff97551775
jezinka <jezina1989@gmail.com>
2017-12-03 11:41:50 +0100

    4.js

---
+++ 4.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+var buf = fs.readFile(process.argv[2], (err, data) => {
+    if (err) throw err;
+    var fileLength = data.toString().split('\n').length - 1;
+    console.log(fileLength);
+  });
+
